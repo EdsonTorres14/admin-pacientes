af6f84e3744029dd2e6a98cfa531137615cf2cff
Edson Torres <66502687+EdsonTorres14@users.noreply.github.com>
2024-04-05 19:05:53 -0600

    84. Creando un componente para mostrar las alertas

--- src/components/Alerta.vue
+++ src/components/Alerta.vue
@@ -1,8 +1,13 @@
 <script setup>
-
+const props = defineProps({
+    alerta: {
+        type: Object,
+        required: true
+    }
+})
 </script>
 <template>
-    <div>
-
+    <div class="text-white text-center p-3 uppercase font-bold rounded-md bg-red-500">
+        {{ alerta.mensaje }}
     </div>
 </template>
